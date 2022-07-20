ca19f2a65db2a40c1146c1baca09e778f3db61b5
Tomas Paoletti Velado <tomas.paolettiv@hotmail.com>
2022-07-20 15:44:16 +0200

    Incorporando objetos y arrays

--- JS/script.js
+++ JS/script.js
@@ -1,33 +1,71 @@
 /* Simulador de creditos */
 
+function ingresopersona() {
+    for (let i = 0; i < 1; i++) {
+        nombrePersona = prompt("Nombre")
+        edadPersona = parseInt(prompt("Edad"));
+        nacionalidad = prompt("Nacionalidad");
+        razonSocial = prompt("Razon social");
+    }
+}
+
 function ingresardinero(dineroprestado) {
     alert("Te prestaremos $" + dineroprestado);
     return dineroprestado
 }
 
-let dineroprestado = parseInt(prompt("Cuanto dinero quieres?"));
-
-ingresardinero(dineroprestado);
-
 function cuotas(cuotasapagar) {
-    if (cuotasapagar <= 12) {
-        alert("Elegiste devolver el dinero en " + cuotasapagar + " meses. El interes es de 10% por cuota");
-    } else {
-        alert("ERROR. Solo puedes devolverlo en 12 meses");
-        let cuotasapagar = prompt("¿En cuantas cuotas quieres devolver el dinero? Puede hasta 12 meses");
+    for (let i = 0; i = cuotasapagar; i++) {
+        if (cuotasapagar <= 12) {
+            alert("Elegiste devolver el dinero en " + cuotasapagar + " meses. El interes es de 10% por cuota");
+            break;
+        } else {
+            alert("ERROR. Solo puedes devolverlo en 12 meses");
+            cuotasapagar = parseInt(prompt("¿En cuantas cuotas quieres devolver el dinero? Puede hasta 12 meses"));
+        }
     }
     return cuotasapagar
 }
 
+function devolucion(dineroconinteres, porcuota) {
+    alert("Tendras que devolver $" + dineroconinteres + " en total.");
+    alert("Tu cuota mensual es de $" + porcuota);
+}
+class PersonaSolicitante {
+    constructor(nombre, edad, nacionalidad, razonSocial) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.nacionalidad = nacionalidad;
+        this.razonSocial = razonSocial;
+    }
+}
+
+class cantidadPrestada {
+    constructor(dineroprestado, cuotasapagar, dineroconinteres, porcuota) {
+        this.dineroprestado = dineroprestado;
+        this.cuotasapagar = cuotasapagar;
+        this.dineroconinteres = dineroconinteres;
+        this.porcuota = porcuota;
+    }
+}
+
+ingresopersona();
+const clientes = [];
+clientes.push( personaIngresada = new PersonaSolicitante(nombrePersona, edadPersona, nacionalidad, razonSocial));
+console.log(personaIngresada);
+
+let dineroprestado = parseInt(prompt("Cuanto dinero quieres?"));
+ingresardinero(dineroprestado);
+
 let cuotasapagar = parseInt(prompt("¿En cuantas cuotas quieres devolver el dinero? Puede hasta 12 meses"));
 cuotas(cuotasapagar);
 
-function devolucion(){
-    const interes = cuotasapagar * 10;
-    const dineroconinteres = dineroprestado + (dineroprestado * (interes / 100));
-    alert("Tendras que devolver $" + dineroconinteres + " en total.");
-    const porcuota = dineroconinteres / cuotasapagar;
-    alert("Tu cuota mensual es de $" + porcuota);
-}
+const interes = cuotasapagar * 10;
+let dineroconinteres = dineroprestado + (dineroprestado * (interes / 100));
+let porcuota = dineroconinteres / cuotasapagar;
+devolucion(dineroconinteres, porcuota);
 
-devolucion();
+clientes.push(prestamoPersonal = new cantidadPrestada(dineroprestado, cuotasapagar, dineroconinteres, porcuota));
+console.log(prestamoPersonal);
+
+console.log(clientes);
